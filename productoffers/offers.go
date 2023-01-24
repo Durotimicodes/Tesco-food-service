afcb15eb6d2601801cecb74aae73b78258c8ebab
durotimicodes <edmondfagbuyi@gmail.com>
2023-01-24 04:57:59 +0000

    implementing buy-one-get-one-free function and methods

--- productoffers/offers.go
+++ productoffers/offers.go
@@ -1,13 +1,47 @@
 package productoffers
 
-//BuyOneGetOneFree takes in a product code, quantity and price and checks if product has a special offer
-func BuyOneGetOneFree(prodQuantity uint, prodCode string, prodPrice float64) bool {
+import "github.com/moretonb/moj-durotimicodes-challenge/models"
 
-	return false
+// BuyOneGetOneFree takes in a product code, quantity and price and checks if product has a special offer
+func BuyOneGetOneFree(prodQuantity uint, prodCode string, prodPrice float64) models.Product {
+	var newProductQuantity, productID uint
+	var sOffer bool
+	var prodName string
+
+	//get product name from map
+	for k, v := range ProductNameAndCode {
+		if v == prodCode {
+			prodName = k
+		}
+	}
+
+	//get product price from function
+	pPrice := GetProductPriceByCode(prodCode)
+
+	//checks
+	for i := 0; i < len(ProductsOnOffer); i++ {
+		//if product is found in the list of offers and the prices are similar add an extra one on each product else just return product price
+		if prodName == ProductsOnOffer[i] && prodPrice == pPrice {
+			newProductQuantity = prodQuantity + prodQuantity
+			sOffer = true
+		} else {
+			newProductQuantity += prodQuantity
+		}
+	}
+
+	//populate the values with product model
+	prd := models.Product{
+		ProductId:        productID,
+		ProductQuantity:  newProductQuantity,
+		ProductCode:      prodCode,
+		HaveSpecialOffer: sOffer,
+		ProductName:      prodName,
+	}
+
+	return prd
 }
 
-
-//DiscountOnAProduct takes in a product code, quantity and price and checks if product has a discount offer
+// DiscountOnAProduct takes in a product code, quantity and price and checks if product has a discount offer
 func DiscountOnAProduct(prodQuantity uint, prodCode string, prodPrice float64) bool {
 
 	return false
--- productoffers/product_offers.go
+++ productoffers/product_offers.go
@@ -1,1 +1,61 @@
 package productoffers
+
+import "github.com/moretonb/moj-durotimicodes-challenge/models"
+
+// product type specification
+type ByOneGetOneSpecification struct {
+	Product models.Product
+}
+
+// one product is satisfied
+func (b *ByOneGetOneSpecification) IsSatisfied(productes *models.Product) bool {
+
+	//get Product name
+	productNames := GetProductNameByCode(b.Product.ProductCode)
+
+	//if product contains in the list of special offers return satisfied else return false
+	if _, exist := SpecialOffers[productNames]; exist {
+		return true
+	}
+
+	return false
+}
+
+type BuyOneGetOneSettings struct{}
+
+func (f *BuyOneGetOneSettings) CheckBuyOneGetOneFree(product []models.Product, spec models.OfferSpecification) []models.Product {
+	result := make([]models.Product, 0)
+
+	//get Product name and Product Price
+	for _, v := range product {
+		if spec.IsSatisfied(&v) {
+			s := BuyOneGetOneFree(v.ProductQuantity, v.ProductCode, v.ProductPrice)
+			result = append(result, s)
+		}
+	}
+
+	return result
+
+}
+
+// product discount specification
+type ProductDiscountSpecification struct {
+	Product models.Product
+}
+
+func (d *ProductDiscountSpecification) IsSatisfied(productes *models.Product) bool {
+
+	//get Product name
+	productNames := GetProductNameByCode(d.Product.ProductCode)
+	productPrice := GetProductPriceByCode(d.Product.ProductCode)
+
+	//if product contains in the list of special offers return satisfied else return false
+	if _, exist := SpecialOffers[productNames]; exist {
+		return true
+	}
+	if _, exist := ProductNameAndPrice[productPrice]; exist {
+		return true
+	}
+
+	return false
+}
